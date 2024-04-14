620a6b3094d1b5f91ef3db7fc332f28ffb2338b9
mgdemel <mgdemel@gmail.com>
2024-04-14 12:14:42 +0300

    add radix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }

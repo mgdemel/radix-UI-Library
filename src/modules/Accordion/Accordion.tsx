34f3953344826700276adbf6a4c9b5d5553d5d2a
mgdemel <mgdemel@gmail.com>
2024-06-11 14:27:59 +0300

    hotfix: remove duplicate button from DOM by removing Radix components from accordion module

--- src/modules/Accordion/Accordion.tsx
+++ src/modules/Accordion/Accordion.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import * as RadixAccordion from "@radix-ui/react-accordion";
-import { IconButton, ChevronDownIcon } from "@radix-ui/themes";
 
 type Props = {
   trigger: React.ReactNode;
@@ -19,10 +18,22 @@ export const Accordion = ({ trigger, content }: Props) => {
       <RadixAccordion.Item className="py-2 w-full" value="item-1">
         <RadixAccordion.Header className="py-2">
           <RadixAccordion.Trigger className="flex">
-            <IconButton className="mr-2">
-              <ChevronDownIcon aria-hidden />
-            </IconButton>
             {trigger}
+            <svg
+              className="m-1"
+              width="24"
+              height="24"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </RadixAccordion.Trigger>
         </RadixAccordion.Header>
         <RadixAccordion.Content className="py-2 overflow-hidden transition-all duration-300 ease-in-out">

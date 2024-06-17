b05e5b4d71eb9ac93fdb668aaa9796f9798f7f23
mgdemel <mgdemel@gmail.com>
2024-06-17 17:54:52 +0300

    update PrimaryButton

--- src/components/Buttons/PrimaryButton/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -2,17 +2,17 @@ import { Button } from "@radix-ui/themes";
 
 type Props = {
   text: string;
+  size?: "1" | "2" | "3" | "4";
   customStyles?: string;
 };
 
-export const PrimaryButton = ({ text, customStyles }: Props) => {
-  const baseStyles =
-    "py-2 px-4 rounded font-medium text-white focus:outline-none ";
-  const primary = " ";
+export const PrimaryButton = ({ text, size, customStyles }: Props) => {
+  const baseStyles = "py-2 px-4 rounded font-medium focus:outline-none ";
   return (
     <Button
+      size={size}
       variant="solid"
-      className={`${baseStyles + (customStyles ?? primary)}`}
+      className={`${baseStyles + customStyles}`}
     >
       {text}
     </Button>

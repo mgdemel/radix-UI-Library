f5cc1ffb35b7e1f1829491b79d283f23ffa54243
mgdemel <mgdemel@gmail.com>
2024-06-17 16:51:42 +0300

    change title component to heading component + make it support dynamic heading levels and styles

--- src/sections/CallToActionBanner/CallToActionBanner.tsx
+++ src/sections/CallToActionBanner/CallToActionBanner.tsx
@@ -1,10 +1,10 @@
-import { Paragraph, PrimaryButton, Title } from "@/components";
+import { Paragraph, PrimaryButton, Heading } from "@/components";
 
 export const CallToActionBanner = () => {
   return (
     <section className="p-10 w-full">
       <div className="mx-auto flex-col flex items-center justify-center gap-1">
-        <Title text="CTA title" />
+        <Heading level="h1" size="9" text="CTA Heading" />
         <Paragraph text="CTA paragraph" />
         <PrimaryButton text="CTA button" />
       </div>

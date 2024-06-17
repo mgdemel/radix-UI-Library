25f987f38e5131baa0ac309e04c062203a6e50eb
mgdemel <mgdemel@gmail.com>
2024-06-17 16:32:04 +0300

    undo revert to pre-main code

--- src/components/TextContent/Title/Title.tsx
+++ src/components/TextContent/Title/Title.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 export const Title = ({ text, customStyles }: Props) => {
-  const baseStyles = "m-1";
+  const baseStyles = "m-1 ";
   const primary = " ";
   return (
     <Heading as="h1" className={`${baseStyles + (customStyles ?? primary)}`}>

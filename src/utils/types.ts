259595d6a79c9a18da9e18f48b8aa736188569b3
mgdemel <mgdemel@gmail.com>
2024-08-20 13:57:21 +0300

    add types for dynamic props

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+export type ButtonSize = "1" | "2" | "3" | "4";
+
 export type Color =
   | "gray"
   | "gold"
@@ -25,3 +27,13 @@ export type Color =
   | "lime"
   | "mint"
   | "sky";
+
+export type DetailSize = "1" | "2" | "3";
+
+export type Radius = "small" | "medium" | "large" | "full";
+
+export type TextAlign = "left" | "center" | "right";
+export type TextLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export type TextSize = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9";
+export type TextTag = "span" | "div" | "label" | "p";
+export type TextWeight = "light" | "regular" | "medium" | "bold";

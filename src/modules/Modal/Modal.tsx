c46b87927865e8b61aacfe871ed698cbdea98abc
mgdemel <mgdemel@gmail.com>
2024-08-20 17:05:20 +0300

    apply theme to modal portal

--- src/modules/Modal/Modal.tsx
+++ src/modules/Modal/Modal.tsx
@@ -1,7 +1,8 @@
+import { Theme } from "@/components/Theme";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross1Icon } from "@radix-ui/react-icons";
 import { VisuallyHidden } from "@radix-ui/themes";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 type Props = {
   modalTitle: string;
@@ -11,16 +12,18 @@ type Props = {
 export const ModalContent = ({ modalTitle, children }: Props) => {
   return (
     <Dialog.Portal>
-      <Dialog.Overlay className="fixed inset-0 bg-black/50" />
-      <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8">
-        <VisuallyHidden asChild>
-          <Dialog.Title>{modalTitle}</Dialog.Title>
-        </VisuallyHidden>
-        <Dialog.Close aria-label="Close">
-          <Cross1Icon />
-        </Dialog.Close>
-        {children}
-      </Dialog.Content>
+      <Theme>
+        <Dialog.Overlay className="fixed inset-0 bg-black/50" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 w-full max-w-md -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-8">
+          <VisuallyHidden asChild>
+            <Dialog.Title>{modalTitle}</Dialog.Title>
+          </VisuallyHidden>
+          <Dialog.Close aria-label="Close">
+            <Cross1Icon />
+          </Dialog.Close>
+          {children}
+        </Dialog.Content>
+      </Theme>
     </Dialog.Portal>
   );
 };

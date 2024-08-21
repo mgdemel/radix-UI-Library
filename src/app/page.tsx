59c0d91c1e860a0915068b746cc3d4f9a666a8a0
Melissa <mgdemel@gmail.com>
2024-08-21 14:26:17 +0300

    add TabBar story + remove example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 "use client";
-import { Text } from "@/components";
-import { TabBar, TabBarItem } from "@/modules";
 import { CallToActionBanner } from "@/sections";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
-      <div className="my-12 w-1/3">
-        <TabBar defaultValue="tab 1" label="tab bar">
-          <TabBarItem value="tab 1">
-            <Text text="Here is a paragraph in tab 1" type="p" />
-          </TabBarItem>
-          <TabBarItem value="tab 2">
-            <Text text="Here is a paragraph in tab 2" type="p" />
-          </TabBarItem>
-          <TabBarItem value="tab 3">
-            <Text text="Here is a paragraph in tab 3" type="p" />
-          </TabBarItem>
-        </TabBar>
-      </div>
-
       <div className="my-12 w-full">
         <CallToActionBanner />
       </div>

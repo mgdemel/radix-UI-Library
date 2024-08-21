b388a266e7fe86c5fa337d20c5cc34bc3577b603
Melissa <mgdemel@gmail.com>
2024-08-21 14:33:08 +0300

    add CallToAction story + remove example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 "use client";
-import { CallToActionBanner } from "@/sections";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full">
-      <div className="my-12 w-full">
-        <CallToActionBanner />
-      </div>
-    </main>
-  );
+  return <main className="flex w-full"></main>;
 }
---
+++ src/sections/CallToActionBanner/__docs__/CallToAction.stories.tsx
@@ -0,0 +1,22 @@
+import { Theme } from "@/components/Theme";
+import type { Meta, StoryObj } from "@storybook/react";
+import { CallToActionBanner } from "../CallToActionBanner";
+
+const meta: Meta<typeof CallToActionBanner> = {
+  title: "Sections/CallToActionBanner",
+  component: CallToActionBanner,
+  decorators: [
+    (Story) => (
+      <Theme>
+        <Story />
+      </Theme>
+    )
+  ]
+};
+export default meta;
+
+type Story = StoryObj<typeof CallToActionBanner>;
+
+export const callToActionBanner: Story = {
+  args: {}
+};

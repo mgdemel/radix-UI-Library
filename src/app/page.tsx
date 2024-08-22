3f57e88a771a2de90446893321f580b8556e2594
Melissa <mgdemel@gmail.com>
2024-08-22 18:05:13 +0300

    remove example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
 "use client";
 
-import { toggleButton } from "@/components";
-import { ArrowDownIcon } from "@radix-ui/react-icons";
-import { IconButton } from "@radix-ui/themes";
-import { useState } from "react";
-
 export default function Home() {
-  const [isOn, setOn] = useState<boolean>(false);
-  const toggle = () => setOn((isOn) => !isOn);
-
-  return (
-    <main className="flex w-full p-10">
-      <IconButton variant={toggleButton(isOn)} onClick={toggle}>
-        <ArrowDownIcon />
-      </IconButton>
-    </main>
-  );
+  return <main className="flex w-full p-10"></main>;
 }

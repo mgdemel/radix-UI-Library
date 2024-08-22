792e5ef73405834022364edc1d32045b9b8122bc
Melissa <mgdemel@gmail.com>
2024-08-22 18:14:42 +0300

    refactor Radio

--- src/components/Inputs/Radio/Radio.tsx
+++ src/components/Inputs/Radio/Radio.tsx
@@ -1,5 +1,5 @@
 import { DetailSize } from "@/utils/types";
-import { Radio as RadixRadio, Text as RadixText } from "@radix-ui/themes";
+import { Flex, Radio as RadixRadio, Text } from "@radix-ui/themes";
 
 type Props = {
   value: string;
@@ -8,14 +8,11 @@ type Props = {
 };
 export const Radio = ({ value, label, size }: Props) => {
   return (
-    <RadixText
-      as="label"
-      size={size}
-      trim="both"
-      className="flex gap-2 items-center leading-none"
-    >
-      <RadixRadio value={value} size={size} variant="surface" highContrast />
-      {label}
-    </RadixText>
+    <Flex asChild gap="2">
+      <Text as="label" size={size} trim="both">
+        <RadixRadio value={value} size={size} />
+        {label}
+      </Text>
+    </Flex>
   );
 };
